d5ce141f5ca855e07b136623792171efc6989afe
nicolasariza <nicolas_ariza@outlook.com>
2020-03-30 21:43:44 -0500

    Updates

---
+++ precache-manifest.0fcac4210e99809d98aed836f1b357ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456b179191e3ef4d0190e4f580602b1d",
+    "url": "/administradorDeTareas/index.html"
+  },
+  {
+    "revision": "43dd79cb6a2db35ed694",
+    "url": "/administradorDeTareas/static/css/main.493a7e67.chunk.css"
+  },
+  {
+    "revision": "3f245d749fbfb1d56cb4",
+    "url": "/administradorDeTareas/static/js/2.b3c3ac82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/administradorDeTareas/static/js/2.b3c3ac82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dd79cb6a2db35ed694",
+    "url": "/administradorDeTareas/static/js/main.a98cf828.chunk.js"
+  },
+  {
+    "revision": "31268557a2d166439da4",
+    "url": "/administradorDeTareas/static/js/runtime-main.b4e43d0b.js"
+  }
+]);
